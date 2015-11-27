151aaa6efd1dd4297ca4be5319103cee8b4d48a2
Tanner Rogalsky <tanner@tannerrogalsky.com>
2015-11-27 10:48:28 -0500

    draw all number cards

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+var ctx = document.getElementById("canvas").getContext("2d");
 var RED = 'red';
 var BLACK = 'black';
+ctx.font = '2px serif';
+ctx.lineWidth = 0.5;
 
 var normalizeData = function(data, x1, y1, x2, y2) {
   for (i = 0; i < data.length;) {
@@ -23,26 +24,55 @@ var drawWrapper = function(color, data, drawer) {
   }
 }
 
-var draw = function(data){
+var drawSuit = function(data){
   ctx.moveTo(data[0], data[1]);
   for (i = 2; i < data.length;) {
     ctx.bezierCurveTo(data[i++], data[i++], data[i++], data[i++], data[i++], data[i++]);
   }
 }
 
+var drawCard = function(number, x, y, s, suitDrawer) {
+  ctx.save();
+  ctx.transform(s, 0, 0, s, x, y);
+
+  ctx.strokeRect(0, 0, 10, 14);
+
+  ctx.fillText(number, 0.5, 2);
+  suitDrawer(0.5, 2, 1);
+
+  var offsetY = 10 / number;
+  for (j = 0; j < number;) {
+    if (number % 2) {
+      suitDrawer(4, offsetY * ++j, 2);
+    } else {
+      suitDrawer(2, offsetY * ++j, 2);
+      suitDrawer(6, offsetY * (++j - 1), 2);
+    }
+  };
+
+  ctx.transform(-1, 0, 0, -1, 10, 14);
+  ctx.fillText(number, 0.5, 2);
+  suitDrawer(0.5, 2, 1);
+
+  ctx.restore();
+}
+
 var diamondData = normalizeData([64,1029,269,829,404,611,532,402,645,632,807,834,982,1029,795,1223,650,1430,533,1647,404,1424,267,1222,64,1029], 0, 402, 1045, 1647);
-var drawDiamond = drawWrapper(RED, diamondData, draw);
+var drawDiamond = drawWrapper(RED, diamondData, drawSuit);
 
 var clubData = normalizeData([522,1635,584,1512,629,1385,635,1251,643,1200,612,1200,609,1237,522,1568,56,1478,73,1164,87,911,340,855,472,939,522,970,519,950,491,919,295,700,416,419,680,414,966,430,1039,734,865,911,842,930,795,1001,865,950,1045,826,1289,961,1283,1155,1275,1512,840,1540,744,1239,736,1208,710,1192,719,1248,730,1349,758,1503,834,1635], 73, 414, 1283, 1635);
-var drawClub = drawWrapper(BLACK, clubData, draw);
+var drawClub = drawWrapper(BLACK, clubData, drawSuit);
 
 var heartData = normalizeData([330,404,490,394,573,530,589,638,599,655,605,650,608,637,616,499,740,386,854,401,1040,404,1153,611,1122,756,1076,971,967,1100,880,1233,781,1370,648,1569,605,1664,605,1664,470,1459,294,1217,176,1055,84,879,73,728,63,586,134,431,330,404], 73, 401, 1122, 1664);
-var drawHeart = drawWrapper(RED, heartData, draw);
+var drawHeart = drawWrapper(RED, heartData, drawSuit);
 
 var spadeData = normalizeData([493,1252,493,1213,459,1213,462,1241,459,1387,334,1460,242,1444,102,1416,58,1278,60,1197,63,944,318,759,547,437,714,728,990,957,1006,1181,1030,1481,649,1592,594,1244,592,1221,563,1207,566,1244,573,1338,560,1380,670,1613,670,1613,388,1613,388,1613,451,1504,488,1369,493,1252], 60, 437, 1006, 1613);
-var drawSpade = drawWrapper(BLACK, spadeData, draw);
+var drawSpade = drawWrapper(BLACK, spadeData, drawSuit);
 
-drawDiamond(100, 100, 100)
-drawSpade(0, 100, 100);
-drawClub(0, 200, 100)
-drawHeart(100, 200, 100)
+for (n = 1; n < 11; n++) {
+  x = (n - 1) * 100
+  drawCard(n, x, 0, 10, drawClub);
+  drawCard(n, x, 140, 10, drawDiamond);
+  drawCard(n, x, 280, 10, drawSpade);
+  drawCard(n, x, 420, 10, drawHeart);
+};
